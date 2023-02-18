1447297246b4487ec1a78b7ac22cf1145055ab2a
mukhammadali010 <muhammadalinosirov34@gmail.com>
2023-02-18 11:47:45 +0500

    fixed

--- src/context/properties/index.jsx
+++ src/context/properties/index.jsx
@@ -1,9 +1,10 @@
 import { createContext, useReducer } from 'react';
+import { reducer } from './reducer';
 
 const PropertiesContext = createContext();
 
 const PropertiesProvider = ({ children }) => {
-    [state , dispatch] = useReducer(()=>{} , [])
+    [state , dispatch] = useReducer(reducer, [])
   return <PropertiesContext.Provider>{children}</PropertiesContext.Provider>;
 };
 
