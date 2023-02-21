85a79f4308eca5740fff5fb4070f4d314454d6fe
mukhammadali010 <muhammadalinosirov34@gmail.com>
2023-02-21 09:48:33 +0500

    finished

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import Button from "../../Generics/Button";
+import Input from "../../Generics/Input";
+import { Container } from "./style";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <Container>
+      <Input width={"829"} />
+      <Button width={'131'} type={'secondary'} padding={'12'}>Advanced</Button>
+      <Button  width={'180'} padding={'12'}>Search</Button>
+    </Container>
+  );
+};
 
-export default Home
+export default Home;
--- src/utils/navbar.js
+++ src/utils/navbar.js
@@ -1,6 +1,7 @@
 import useUniqueId from '../hooks/index.jsx'
 import Home from '../pages/HomePage/HomePage.jsx'
 import PropertiesPage from '../pages/PropertiesPage/PropertiesPage.jsx'
+import LoginPage from '../pages/LoginPage/LoginPage.jsx'
 export const navbar = [
     {
         id: useUniqueId,
@@ -17,6 +18,14 @@ export const navbar = [
         element: <PropertiesPage /> ,
         isPrivate: false,
         isHidden: false,
+    },
+    {
+        id: useUniqueId,
+        path: 'login',
+        title: 'Login',
+        element: <LoginPage /> ,
+        isPrivate: false,
+        isHidden: true,
     },
 ]
     
